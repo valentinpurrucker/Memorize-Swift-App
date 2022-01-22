c3cfa204f1be125cd6e3c10eb1fc58d0ca178b91
Valentin Purrucker <vali.pur13@gmail.com>
2022-01-22 11:32:44 +0100

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Valentin Purrucker on 22.01.22.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
